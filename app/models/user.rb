5f9d4ab1c7794d5ae2b1a9ecdd92e270d54afdb2
George Zhukov <scofield.cross@gmail.com>
2015-08-01 04:14:59 +0300

    added user avatar url method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,22 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      # user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+      user.avatar = auth.info.image
+    end
+  end
+
+  def cart
+    self.orders.in_progress.first_or_create
+  end
+
+  def avatar_url
+    if self.avatar
+      self.avatar
+    else
+      gravatar_id = Digest::MD5.hexdigest(email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=128"
     end
   end
 end
