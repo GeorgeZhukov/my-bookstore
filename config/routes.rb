13fd563965441d24a8d87f770067b89a2f2fe67c
George Zhukov <scofield.cross@gmail.com>
2015-08-01 04:06:00 +0300

    configured basic cloudinary support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'welcome/index'
+  post 'welcome/test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
