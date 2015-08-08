6a71a05df704dbe2b013ff2fa4f6b7bd0fcc1ee9
George Zhukov <scofield.cross@gmail.com>
2015-08-08 18:27:24 +0300

    removed deprecated application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def form_errors(obj)
-    result = ""
-    if obj.errors.any?
-      inner_html = ""
-      obj.errors.full_messages.each.with_index do |msg, index|
-        inner_html += "%d. %s <br/>" % [index+1, msg]
-      end
-      result = '<div class="alert alert-danger" role="alert">%s</div>' % inner_html
-    end
-    return result
-  end
 end
