6341f05815215e4c1ffe21773c63950756699a08
George Zhukov <scofield.cross@gmail.com>
2015-08-10 17:03:36 -0400

    ..

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,16 +8,10 @@ class RegistrationsController < Devise::RegistrationsController
 
   def update
     if params[:billing_address]
-      current_user.billing_address ||= Address.new
-      is_updated = current_user.billing_address.update address_params(:billing_address)
-      flash[:notice] = "The billing address is saved." if is_updated
-      return respond_with current_user.billing_address unless is_updated
+      return respond_with current_user.billing_address unless update_billing_address
     end
     if params[:shipping_address]
-      current_user.shipping_address ||= Address.new
-      is_updated = current_user.shipping_address.update address_params(:shipping_address)
-      flash[:notice] = "The shipping address is saved." if is_updated
-      return respond_with current_user.shipping_address unless is_updated
+      return respond_with current_user.shipping_address unless update_shipping_address
     end
 
     if params[:billing_address] or params[:shipping_address]
@@ -30,6 +24,20 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   private
+
+  def update_billing_address
+    current_user.billing_address ||= Address.new
+    is_updated = current_user.billing_address.update address_params(:billing_address)
+    flash[:notice] = "The billing address is saved." if is_updated
+    is_updated
+  end
+
+  def update_shipping_address
+    current_user.shipping_address ||= Address.new
+    is_updated = current_user.shipping_address.update address_params(:shipping_address)
+    flash[:notice] = "The shipping address is saved." if is_updated
+    is_updated
+  end
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
