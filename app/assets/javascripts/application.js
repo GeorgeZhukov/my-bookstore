df63331f7ed4cb210f50640ce43e676929c7e2c8
George Zhukov <scofield.cross@gmail.com>
2015-08-12 06:13:52 +0300

    added bounce.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,77 @@
 //= require slick
 //= require jquery.raty
 //= require intlTelInput
+//= require bounce.min
 //= require_tree .
+
+var logoBounce = new Bounce();
+logoBounce.
+    translate({
+        from: {x: -300, y: 0},
+        to: {x: 0, y: 0},
+        duration: 600,
+        stiffness: 4
+    })
+    .scale({
+        from: {x: 1, y: 1},
+        to: {x: 0.1, y: 2.3},
+        easing: "sway",
+        duration: 800,
+        delay: 65,
+        stiffness: 2
+    })
+    .scale({
+        from: {x: 1, y: 1},
+        to: {x: 5, y: 1},
+        easing: "sway",
+        duration: 300,
+        delay: 30,
+    });
+
+var navbarBounce = new Bounce();
+navbarBounce.
+    translate({
+        from: {x: 300, y: 0},
+        to: {x: 0, y: 0},
+        duration: 600,
+        stiffness: 4
+    })
+    .scale({
+        from: {x: 1, y: 1},
+        to: {x: 0.1, y: 2.3},
+        easing: "sway",
+        duration: 800,
+        delay: 65,
+        stiffness: 2
+    })
+    .scale({
+        from: {x: 1, y: 1},
+        to: {x: 5, y: 1},
+        easing: "sway",
+        duration: 300,
+        delay: 30,
+    });
+
+var bookBounce = new Bounce();
+bookBounce.
+    skew({
+        from: {x: 0, y: 0},
+        to: {x: 40, y: 60},
+        easing: "sway",
+        duration: 750,
+        bounces: 4
+    }).scale({
+        from: {x: 0.5, y: 0.5},
+        to: {x: 1, y: 1},
+        easing: "bounce",
+        duration: 750,
+        bounces: 4
+    });
+
+
+$(function () {
+    // Bounce logo
+    logoBounce.applyTo($("#logo"));
+    bookBounce.applyTo($(".book"));
+    navbarBounce.applyTo($(".navbar-right"));
+});
