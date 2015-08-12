c76eff63f0d43cbf844c899d8141bf8f75f2578f
George Zhukov <scofield.cross@gmail.com>
2015-08-13 02:26:13 +0300

    added one test for omniauth controller

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
+  before do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
 
+  let(:user) { FactoryGirl.create :facebook_user }
+
+  it "calls User.from_omniauth" do
+    expect(User).to receive(:from_omniauth).and_return(user)
+    get :facebook
+  end
+
+  it "redirect to sign up page if given information not enough"
 end
