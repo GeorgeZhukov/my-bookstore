db643a5c5d67c513bf42a5cdc1d2298a70dc0c61
George Zhukov <scofield.cross@gmail.com>
2015-08-14 23:39:08 +0300

    updated rating styles

--- spec/views/books/show.html.haml_spec.rb
+++ spec/views/books/show.html.haml_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "books/show.html.haml", type: :view do
   let(:book) { FactoryGirl.create :book }
+  let(:rating) { FactoryGirl.create(:rating) }
+
+  before do
+    assign(:book, book)
+    assign(:new_rating, rating)
+  end
 
   it "displays the book" do
-    assign(:book, book)
-    assign(:new_rating, FactoryGirl.create(:rating))
     render
     expect(rendered).to match(book.title)
   end
 
   it "displays the ratings" do
-    assign(:book, book)
-    assign(:new_rating, FactoryGirl.create(:rating))
     render
     expect(rendered).to match(book.title)
+  end
+
+  it "displays a link to the author" do
+    render
+    expect(rendered).to match(book.author.to_s)
   end
 end
