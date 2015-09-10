c7c07ec94278b94ada8bf9d652082371fdf10364
George Zhukov <scofield.cross@gmail.com>
2015-09-10 13:14:07 +0300

    ..

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe RatingsController, type: :controller do
-  # login_user
-  #
-  # describe "GET index" do
-  #   it "assigns @books" do
-  #     book = create :book
-  #     @user.get_wish_list.books << book
-  #     get :index
-  #     expect(assigns(:books)).to eq [book]
-  #   end
-  #
-  #   it "renders the index template" do
-  #     get :index
-  #     expect(response).to render_template("index")
-  #   end
-  # end
+  login_user
+
+  before do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    allow(@controller).to receive(:current_ability).and_return(@ability)
+    @ability.can :manage, :all
+  end
+
+  describe "POST #create" do
+    let(:book) { create :book }
+    let(:rating_attrs) { attributes_for :rating }
+
+    it "redirect to book path when creates a new rating" do
+      post :create, book_id: book.id, rating: rating_attrs
+      expect(response).to redirect_to book
+    end
+
+    context "cancan doesn't allow create" do
+      before do
+        @ability.cannot :create, Rating
+      end
+
+      it {expect { post :create, book_id: book.id, rating: rating_attrs }.to raise_error(CanCan::AccessDenied)}
+    end
+  end
 end
